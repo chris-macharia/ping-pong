f1f437d0ab0178a2d140e38e64e8850cee96b092
Chris Macharia <christophermaina003@gmail.com>
2023-09-30 08:49:45 +0300

    Define the 2 paddles and a ball

--- pong1.py
+++ pong1.py
@@ -10,6 +10,32 @@ win.setup(width=800, height=600)
 win.tracer(0)
 
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("White")
+paddle_b.shapesize(stretch_len=1, stretch_wid=5)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("White")
+ball.penup()
+ball.goto(0, 0)
+
 # Main game loop
 while True:
     win.update()
